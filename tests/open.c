5bf2a903dd10ff45b39b8479c4d09fa55e3c4773
Dmitry V. Levin <ldv@altlinux.org>
2017-03-26 21:24:00 +0000

    tests: adjust open.test to use $STRACE_TEST_SAMPLE
    * tests/open.c (main): Use get_sample_name() instead of hardcoded value.
    * tests/open.test: Use $STRACE_TEST_SAMPLE instead of hardcoded value.

--- tests/open.c
+++ tests/open.c
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2016 Dmitry V. Levin <ldv@altlinux.org>
+ * Check decoding of open syscall.
+ *
+ * Copyright (c) 2016-2017 Dmitry V. Levin <ldv@altlinux.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -37,7 +39,7 @@
 int
 main(void)
 {
-	static const char sample[] = "open.sample";
+	const char *const sample = get_sample_name();
 
 	long fd = syscall(__NR_open, sample, O_RDONLY|O_CREAT, 0400);
 	printf("open(\"%s\", O_RDONLY|O_CREAT, 0400) = %s\n",
